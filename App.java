064d2df213aab5e4e0ae1196a8850fa9f33cf23c
Manoel Campos <manoelcampos@gmail.com>
2023-10-16 17:21:12 -0300

    Adiciona docs no App.java

--- App.java
+++ App.java
@@ -1,3 +1,7 @@
+/**
+ * Um exemplo de aplicação Java usando os recurso do JDK 21,
+ * que podem ainda não ser suportados pela maioria dos editores e IDEs.
+ */
 void main(){
     System.out.println("Minha Primeira Aplicação Java");
 }
